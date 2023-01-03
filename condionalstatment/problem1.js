15c9f01fb18fe347fd8054bb16ff53dc39752757
abhishekghadge05 <121336464+abhishekghadge05@users.noreply.github.com>
2023-01-03 20:49:05 +0530

    add second day assignment

---
+++ condionalstatment/problem1.js
@@ -0,0 +1,12 @@
+let a=5;
+let b=10;
+let c=20;
+if ((a>b)&&(a>c)){
+  console.log("A is Greater")
+}
+else if ((b>a)&&(b>c)){
+  console.log("B is Greater")
+}
+else if ((c>a)&&(c>b)){
+  console.log("C is Greater")
+}
